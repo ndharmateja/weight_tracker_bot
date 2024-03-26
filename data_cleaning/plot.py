9987246d7c2bb8ed75e31c8c5d3f0f7b102a3ddb
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-03-26 23:31:42 +0530

    refactor into a separate plot module

---
+++ data_cleaning/plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+
+def plot_data(df, img_filename):
+    # plot data and save to file
+    plt.rcParams["figure.figsize"]=(16,7)
+    plt.plot(df["Weight"], color="blue")
+    plt.title("Weight vs Time")
+    plt.grid()
+    plt.yticks(range(84, 110, 2))
+    plt.tick_params(labelleft=True, labelbottom=True, labelright=True)
+    plt.savefig(img_filename)
